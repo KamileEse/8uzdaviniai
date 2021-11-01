cafd56d9a5b1ed8f0e5676edfbe77394ee567902
KamileEse <eselinaitek@gmail.com>
2021-11-01 19:44:56 +0200

    2

---
+++ Task2/Inputter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task2
+{
+    public class Inputter
+    {
+        public void Nuskaitymas()
+        {
+            List<int> manoSarasas = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Iveskite skaiciu {i + 1}");
+                string ivestis = Console.ReadLine();
+                int skaicius = Convert.ToInt32(ivestis);
+                manoSarasas.Add(skaicius); // --Add yra metodas
+            }
+        }
+
+        public bool SarasasTeigiamas(List<int> skaiciuSarasas)
+        {
+            bool rezultatas = true;   // rezultatas bus true, kol nesutiksiu neigiamo. kai
+            //sutiksiu, pakeis i false -->
+
+            if (skaiciuSarasas == null || skaiciuSarasas.Count == 0) // -- kai ne metodas, tai be skliaustu
+            {
+                rezultatas = false;
+            }
+
+            foreach (var skaicius in skaiciuSarasas)
+            {
+                // jei 1 - nieko nedaro
+                // jei -2 - rezultatui priskirs false reiksme
+
+                if (skaicius <= 0)
+                {
+                    rezultatas = false;
+                }
+            }
+            // rezultatas - false
+
+            return rezultatas; // grazina false
+        }
+
+        public void MatematiniaiVeiksmai(List<int> skaiciuSarasas)
+        {
+            var suma = skaiciuSarasas.Count();
+
+            var sandauga = skaiciuSarasas.Aggregate((x, y) => x * y);
+        }
+
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class MainClass
+    {
+        //1. Programa turi paprašyti suvesti 4 teigiamus skaičius
+        //2. Patikrinti, kad visi skaičiai yra teigiami(jeigu ne,
+        //išvesti vartotojui klaidą)
+        //3. Išspausdinti visų skaičių sumą, skirtumą, sandaugą, dalybą,
+        //jeigu visi skaičiai teigiami
+
+        public static void Main(string[] args)
+        {
+            Inputter irasas = new Inputter();
+
+
+            irasas.Nuskaitymas();
+            irasas.SarasasTeigiamas(); // nemoku iskviest
+            
+
+            Console.ReadLine();
+        }
+    }
+}
