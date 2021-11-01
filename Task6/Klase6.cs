12bfefaefe9aeaff39073c0409f4b2841a41496a
KamileEse <eselinaitek@gmail.com>
2021-11-01 19:46:09 +0200

    6

---
+++ Task6/Klase6.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task6
+{
+    public class Klase6
+    {
+        public void PatikrintiArEgzistuoja(string tekstas, string path)
+        {
+            List<string> irasas = new List<string>();
+            irasas = File.ReadAllLines(path).ToList();
+
+            if (irasas.Contains(tekstas))
+            {
+                    Console.WriteLine("Taip");
+            }
+            else
+            {
+                Console.WriteLine("Ne");
+            }
+        }
+
+        public void PridetiIrasa(string tekstas)
+        {
+            try
+            {
+                string path = @"/Users/kamileeselinaite/Desktop/C#/test6.txt";
+                File.WriteAllText(path, tekstas);
+            }
+            catch(DirectoryNotFoundException)
+            {
+                Console.WriteLine("Ivyko klaida, tokia vieta neegzistuoja");
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("ERROR 404");
+            }
+            catch(UnauthorizedAccessException)
+            {
+                Console.WriteLine("Jums nesuteikta teise pasiekti faila");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Klaida");
+            }
+        }
+
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task6
+{
+    //1. Sukurti klasę kuri turėtų du metodus
+    //2. Metodas 1 turėtų patikrinti ar faile egzistuoja įrašas ir gražinti "taip" arba "ne"
+    //3. Metodas 2 turėtų pridėti įrašą į failą
+    //4. Nutikus klaidai, klaida išsaugoma į atskirą failą.
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"/Users/kamileeselinaite/Desktop/C#/test6.txt";
+            string tekstas;
+            tekstas = Console.ReadLine();
+
+            Klase6 naujasIrasas = new Klase6();
+
+            naujasIrasas.PatikrintiArEgzistuoja(path, "lalala");
+            naujasIrasas.PridetiIrasa(path);
+
+            Console.ReadLine();
+        }
+    }
+}
